960d90de5d3b341d10d5e97514e9332df981aacb
Kapizany <kapizany@gmail.com>
2020-03-01 22:00:18 -0300

    Gravação das despesas na página consulta

--- app.js
+++ app.js
@@ -42,6 +42,24 @@ class BD {
         localStorage.setItem(id, JSON.stringify(d))
         localStorage.setItem('id',id)
     }
+
+    recuperarTodosRegistros(){
+
+        let despesas = Array()
+        let id = localStorage.getItem('id')
+
+        for (let counter=1; counter<=id; counter++){
+
+            let despesa = JSON.parse(localStorage.getItem(counter))
+
+            if (!!despesa){ despesas.push(despesa)}
+        }
+        return despesas
+    }
+
+    pesquisar(despesa){
+        
+    }
 }
 
 let bd = new BD()
@@ -64,11 +82,84 @@ function cadastrarDespesa() {
     )
 
     if (despesa.validarDados()){
+        
         bd.gravar(despesa)
-        $('#sucessoGravacao').modal('show')
+        
+        document.getElementById("modalLabel").className="modal-title text-success"
+        document.getElementById("modalLabel").innerText="Registro inserido com sucesso"
+        document.getElementById("modal-text").innerText="Despesa registrada com sucesso!"
+        document.getElementById("voltar").className="btn btn-success"
+        document.getElementById("voltar").innerText="Voltar"
+
+        ano.value = ""
+        mes.value = ""
+        dia.value = ""
+        tipo.value = ""
+        descricao.value = "" 
+        valor.value = ""
+
+        $('#registraDespesa').modal('show')
     }
     else{
-        $('#erroGravacao').modal('show')
+        document.getElementById("modalLabel").className="modal-title text-danger"
+        document.getElementById("modalLabel").innerText="Registro com problemas"
+        document.getElementById("modal-text").innerText="Existem campos não preenchidos!"
+        document.getElementById("voltar").className="btn btn-danger"
+        document.getElementById("voltar").innerText="Voltar e corrigir"
+        $('#registraDespesa').modal('show')
     }
 }
 
+function consultaListaDespesas(){
+
+    let despesas = Array()
+    despesas = bd.recuperarTodosRegistros()
+    var listaDespesas = document.getElementById('listaDespesas')
+
+    despesas.forEach( function (desp){
+        
+        //criando linha tr
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas
+        linha.insertCell(0).innerHTML = `${desp.dia}/${desp.mes}/${desp.ano}`
+        
+        switch(desp.tipo){
+            case '1': desp.tipo = "Alimentação"
+                break
+            case '2': desp.tipo = "Educação"
+                break
+            case '3': desp.tipo = "Lazer"
+                break
+            case '4': desp.tipo = "Saúde"
+                break
+            case '5': desp.tipo = "Transporte"
+                break
+        }
+
+        linha.insertCell(1).innerHTML = desp.tipo
+        linha.insertCell(2).innerHTML = desp.descricao
+        linha.insertCell(3).innerHTML = desp.valor
+
+    })
+}
+
+function pesquisarDespesa(){
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = (
+        ano,
+        mes,
+        dia,
+        tipo,
+        descricao,
+        valor
+    )
+
+    bd.pesquisar(despesa)
+}
